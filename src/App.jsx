97156d14c27b7763b6c97dda93a337a458b7f547
meri0031 <nathan.merieux@etudiant.univ-reims.fr>
2024-02-04 01:45:20 +0100

    add deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,26 @@ function App() {
 
   return (
     <div className="App">
-      <form className="flex flex-col w-36">
-        <input type={'number'} placeholder={'Montant (en €)'} name={'montant'} step={'0.01'} required/>
-        <input type={'date'} placeholder={'Date'} name={'date'} required/>
-        <div className="flex items-center">
-          <input id="option1" type="radio" name="type" value="Dépot" required
-                 className="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"/>
-          <label htmlFor="depot" className="ml-2 text-sm text-gray-700">Dépot</label>
-        </div>
-        <div className="flex items-center">
-          <input id="option2" type="radio" name="type" value="Retrait" required
-                 className="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"/>
-          <label htmlFor="retrait" className="ml-2 text-sm text-gray-700">Retrait</label>
-        </div>
-        <button type={'button'} onClick={ajouterTransaction} className={"bg-red-200" + " rounded-full"}>Ajouter</button>
-      </form>
+      <header className="flex">
+        <form className="flex flex-col">
+          <input type={'number'} placeholder={'Montant (en €)'} name={'montant'} step={'0.01'} required/>
+          <input type={'date'} placeholder={'Date'} name={'date'} required/>
+          <div className="flex items-center">
+            <input id="option1" type="radio" name="type" value="Dépot" required
+                   className="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"/>
+            <label htmlFor="depot" className="ml-2 text-sm text-gray-700">Dépot</label>
+          </div>
+          <div className="flex items-center">
+            <input id="option2" type="radio" name="type" value="Retrait" required
+                   className="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"/>
+            <label htmlFor="retrait" className="ml-2 text-sm text-gray-700">Retrait</label>
+          </div>
+          <button type={'button'} onClick={ajouterTransaction} className={"bg-red-200" + " rounded-full"}>Ajouter
+          </button>
+        </form>
+        <div></div>
+      </header>
+
       <div className={'transactions flex-col-reverse flex p-6 '}>
         {transactions}
       </div>
